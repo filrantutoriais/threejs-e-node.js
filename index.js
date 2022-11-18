9d31fa12f1769de094c2c12b049ec07f34ece78b
Filipe Arantes Fernandes <filran@gmail.com>
2022-11-18 18:25:19 -0300

    arquivos atualizados

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import express from "express";
 import * as path from "path";
-import url from "url";
+import { fileURLToPath } from "url";
 
-const __filename =  url.fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
-app.listen(3000, ()=>{ console.log(`Connected`) });
+app.listen(3000, () =>
+  console.log('Servidor conectado: http://127.0.0.1:3000')
+);
